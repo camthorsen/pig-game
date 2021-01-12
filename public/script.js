163e1e1a87d1ca85cd530b4575983bca8e600269
Camilla Thorsen <cthorsen@skypilot.dev>
2021-01-12 10:41:28 -0800

    feat: On "roll" btn click, create random roll and display dice face img

--- public/script.js
+++ public/script.js
@@ -1,11 +1,26 @@
 'use strict';
 
+const diceEl = document.querySelector('.dice');
+// const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+// const btnHold = document.querySelector('.btn--hold');
+
 // STATE
 let player0scoreEl = document.getElementById('score--0');
 let player1scoreEl = document.getElementById('score--1');
-let diceEl = document.querySelector('.dice');
 
 // FUNCTIONS
+
+function rollDice () {
+  // Generate a random dice roll
+  const rollResult = Math.trunc(Math.random() * 6) + 1;
+
+  // Display corresponding dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${rollResult}.png`;
+
+  // Check for rolled 1: If true, switch to next player
+}
 
 function startGame () {
   player0scoreEl.textContent = 0;
@@ -16,3 +31,5 @@ function startGame () {
 startGame();
 
 // EVENT LISTENERS
+
+btnRoll.addEventListener('click', rollDice);
