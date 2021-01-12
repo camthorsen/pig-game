fb15b9b214f1d419d279441018dea137d20e6833
Camilla Thorsen <cthorsen@skypilot.dev>
2021-01-11 19:10:51 -0800

    feat: Set player scores to 0 by default

--- public/script.js
+++ public/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// STATE
+let player0scoreEl = document.getElementById('score--0');
+let player1scoreEl = document.getElementById('score--1');
+
+// FUNCTIONS
+
+function startGame () {
+  player0scoreEl.textContent = 0;
+  player1scoreEl.textContent = 0;
+}
+
+startGame();
+
+// EVENT LISTENERS
