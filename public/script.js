a9b1e040864c908b8a61a3faf02930240b950313
Camilla Thorsen <cthorsen@skypilot.dev>
2021-01-12 10:16:59 -0800

    feat: Hide dice image by default at start of a new game

--- public/script.js
+++ public/script.js
@@ -3,12 +3,14 @@
 // STATE
 let player0scoreEl = document.getElementById('score--0');
 let player1scoreEl = document.getElementById('score--1');
+let diceEl = document.querySelector('.dice');
 
 // FUNCTIONS
 
 function startGame () {
   player0scoreEl.textContent = 0;
   player1scoreEl.textContent = 0;
+  diceEl.classList.add('hidden');
 }
 
 startGame();
